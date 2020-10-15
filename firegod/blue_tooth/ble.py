10b584daadef4821ecd3adf3655a54f4464c337d
jackbapa <2860671628@qq.com>
2020-10-15 16:28:22 +0800

    3

--- firegod/blue_tooth/ble.py
+++
@@ -1,7 +0,0 @@
-from bluetooth.ble import DiscoveryService
-
-service = DiscoveryService()
-devices = service.discover(2)
-
-for address, name in devices.items():
-    print("Name: {}, address: {}".format(name, address))
